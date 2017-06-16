4075732e607041b55e0afadc4c8a6da5e23d1ab3
Joel Takvorian <jtakvori@redhat.com>
2017-06-16 14:46:33 +0200

    Hawkular + Openshift

--- start.sh
+++ start.sh
@@ -8,5 +8,5 @@
 #
 # echo "Hawkular ready!"
 
-java -Dvertx.disableDnsResolver=true -DHAWKULAR_HOST=$HAWKULAR_HOST -jar falco-demo.jar
+java -Dvertx.disableDnsResolver=true -jar falco-demo.jar
 # java -Dvertx.disableDnsResolver=true -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -DHAWKULAR_HOST=$HAWKULAR_HOST -DUSE_DW_METRICS=true -Dorg.slf4j.simpleLogger.defaultLogLevel=debug -jar falco-demo.jar
