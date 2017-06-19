44a0a22a187b465c684c8d58ea55ac861181615f
Joel Takvorian <jtakvori@redhat.com>
2017-06-19 17:23:36 +0200

    Setup prometheus endpoints & docker-compose

--- src/main/java/org/hawkular/sample/falco/GameMonitoring.java
+++ src/main/java/org/hawkular/sample/falco/GameMonitoring.java
@@ -4,22 +4,20 @@ import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.Random;
 
-import org.hawkular.metrics.client.HawkularClient;
-import org.hawkular.metrics.client.HawkularClientBuilder;
-import org.hawkular.metrics.client.HawkularLogger;
-import org.hawkular.metrics.client.model.Gauge;
-import org.hawkular.metrics.client.model.Logger;
+import com.codahale.metrics.SharedMetricRegistries;
 
 import flying.spaghetti.code.monster.TotalNonSense;
+import io.prometheus.client.CollectorRegistry;
+import io.prometheus.client.Gauge;
+import io.prometheus.client.dropwizard.DropwizardExports;
+import io.prometheus.client.vertx.MetricsHandler;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
 import io.vertx.core.eventbus.EventBus;
 import io.vertx.core.eventbus.Message;
-import io.vertx.core.http.HttpClientOptions;
 import io.vertx.core.json.JsonObject;
-import io.vertx.ext.hawkular.AuthenticationOptions;
-import io.vertx.ext.hawkular.VertxHawkularOptions;
+import io.vertx.ext.dropwizard.DropwizardMetricsOptions;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.StaticHandler;
 import io.vertx.ext.web.handler.sockjs.BridgeOptions;
@@ -29,72 +27,49 @@ import io.vertx.ext.web.handler.sockjs.SockJSHandler;
 public class GameMonitoring extends AbstractVerticle {
 
     private static final Random RANDOM = new Random();
-    private static final String HAWKULAR_SECURED = System.getenv("HAWKULAR_HTTPS");
-    private static final String HAWKULAR_HOST = getEnvOrDefault("HAWKULAR_HOST", "localhost");
-    private static final String HAWKULAR_PORT = getEnvOrDefault("HAWKULAR_PORT", "8080");
-    private static final String HAWKULAR_TENANT = getEnvOrDefault("HAWKULAR_TENANT", "falco");
-    private static final String HAWKULAR_AUTH_TOKEN = System.getenv("HAWKULAR_AUTH_TOKEN");
-    private static final String HAWKULAR_USERNAME = System.getenv("HAWKULAR_USERNAME");
-    private static final String HAWKULAR_PASSWORD = System.getenv("HAWKULAR_PASSWORD");
-    private static final String HAWKULAR_URI = ("true".equals(HAWKULAR_SECURED) ? "https://" : "http://")
-            + HAWKULAR_HOST + ":" + HAWKULAR_PORT;
 
-    private final HawkularClient hawkular;
-    private final HawkularLogger hawkularLogger;
-
-    private static String getEnvOrDefault(String key, String def) {
-        String env = System.getenv(key);
-        if (env == null) {
-            return def;
-        }
-        return env;
-    }
+    private final Gauge score;
+    private final Gauge heat;
+    private final Gauge level;
+    private final Gauge heatLimit;
 
     private GameMonitoring() {
-        hawkular = hawkularBuilder().build();
-        hawkularLogger = hawkularBuilder().buildLogger("falco");
-    }
-
-    private static HawkularClientBuilder hawkularBuilder() {
-        HawkularClientBuilder builder = new HawkularClientBuilder(HAWKULAR_TENANT).uri(HAWKULAR_URI);
-        if (HAWKULAR_AUTH_TOKEN != null) {
-            builder.bearerToken(HAWKULAR_AUTH_TOKEN);
-        } else if (HAWKULAR_USERNAME != null && HAWKULAR_PASSWORD != null) {
-            builder.basicAuth(HAWKULAR_USERNAME, HAWKULAR_PASSWORD);
-        }
-        return builder;
+        CollectorRegistry.defaultRegistry.register(new DropwizardExports(SharedMetricRegistries.getOrCreate("vertx")));
+        score = Gauge.build()
+                .name("falco_score")
+                .help("Player score")
+                .labelNames("player")
+                .register();
+        heat = Gauge.build()
+                .name("falco_heat")
+                .help("Player heat")
+                .labelNames("player")
+                .register();
+        level = Gauge.build()
+                .name("falco_level")
+                .help("Player level")
+                .labelNames("player")
+                .register();
+        heatLimit = Gauge.build()
+                .name("falco_heat_limit")
+                .help("Player maximum heat")
+                .labelNames("player")
+                .register();
     }
 
     public static void main(String[] args) throws InterruptedException {
-        System.out.println("HAWKULAR_HOST=" + HAWKULAR_HOST);
-        System.out.println("HAWKULAR_PORT=" + HAWKULAR_PORT);
-        AuthenticationOptions authenticationOptions = new AuthenticationOptions();
-        VertxHawkularOptions vertxHawkularOptions = new VertxHawkularOptions()
+        DropwizardMetricsOptions dwOptions = new DropwizardMetricsOptions()
                 .setEnabled(true)
-                .setTenant(HAWKULAR_TENANT)
-                .setHost(HAWKULAR_HOST)
-                .setPort(Integer.parseInt(HAWKULAR_PORT));
-        if (HAWKULAR_AUTH_TOKEN != null) {
-            vertxHawkularOptions.setHttpHeaders(new JsonObject()
-                    .put("Authorization", "Bearer " + HAWKULAR_AUTH_TOKEN));
-        } else if (HAWKULAR_USERNAME != null && HAWKULAR_PASSWORD != null) {
-            authenticationOptions = authenticationOptions
-                    .setEnabled(true)
-                    .setId(HAWKULAR_USERNAME)
-                    .setSecret(HAWKULAR_PASSWORD);
-        }
-        if ("true".equals(HAWKULAR_SECURED)) {
-            vertxHawkularOptions.setHttpOptions(new HttpClientOptions().setSsl(true));
-        }
-        VertxOptions options = new VertxOptions().setMetricsOptions(
-                vertxHawkularOptions
-                        .setAuthenticationOptions(authenticationOptions));
-        Vertx.vertx(options).deployVerticle(new GameMonitoring());
+                .setRegistryName("vertx");
+        Vertx.vertx(new VertxOptions().setMetricsOptions(dwOptions)).deployVerticle(new GameMonitoring());
     }
 
     @Override
     public void start() throws Exception {
         Router router = Router.router(vertx);
+
+        // Expose prometheus endpoints
+        router.get("/prom").handler(new MetricsHandler());
 
         // Allow events for the designated addresses in/out of the event bus bridge
         BridgeOptions opts = new BridgeOptions()
@@ -122,26 +97,26 @@ public class GameMonitoring extends AbstractVerticle {
         EventBus eb = vertx.eventBus();
         eb.consumer("init-session", msg -> {
             String playerName = playerName(msg);
-            score(playerName).set(0);
-            heat(playerName).set(100);
-            level(playerName).set(0);
-            heatLimit(playerName).set(200);
-            timeline(playerName).log("Starting new game");
+            score.labels(playerName).set(0);
+            heat.labels(playerName).set(100);
+            level.labels(playerName).set(0);
+            heatLimit.labels(playerName).set(200);
+//            timeline(playerName).log("Starting new game");
         });
-        eb.consumer("new-score", msg -> score(playerName(msg)).set((int) value(msg)));
-        eb.consumer("heat", msg -> heat(playerName(msg)).set(((Number) value(msg)).doubleValue()));
-        eb.consumer("new-level", msg -> level(playerName(msg)).set((int) value(msg)));
-        eb.consumer("overheated", msg -> timeline(playerName(msg)).log("Overheated :-("));
+        eb.consumer("new-score", msg -> score.labels(playerName(msg)).set((int) value(msg)));
+        eb.consumer("heat", msg -> heat.labels(playerName(msg)).set(((Number) value(msg)).doubleValue()));
+        eb.consumer("new-level", msg -> level.labels(playerName(msg)).set((int) value(msg)));
+//        eb.consumer("overheated", msg -> timeline(playerName(msg)).log("Overheated :-("));
         eb.consumer("fscm", msg -> {
-            timeline(playerName(msg)).log("Careful! Just caught an Flying Spaghetti Code Monster! What could go wrong?");
+//            timeline(playerName(msg)).log("Careful! Just caught an Flying Spaghetti Code Monster! What could go wrong?");
             vertx.executeBlocking(future -> {
                 generateException();
                 future.complete();
             }, res -> {});
         });
-        eb.consumer("nodes", msg -> heatLimit(playerName(msg)).set(200 * (int) value(msg)));
-        eb.consumer("won", msg -> timeline(playerName(msg)).log("WON!"));
-        eb.consumer("game-over", msg -> timeline(playerName(msg)).log("GAME OVER!"));
+        eb.consumer("nodes", msg -> heatLimit.labels(playerName(msg)).set(200 * (int) value(msg)));
+//        eb.consumer("won", msg -> timeline(playerName(msg)).log("WON!"));
+//        eb.consumer("game-over", msg -> timeline(playerName(msg)).log("GAME OVER!"));
     }
 
     private static String playerName(Message msg) {
@@ -150,46 +125,6 @@ public class GameMonitoring extends AbstractVerticle {
 
     private static Object value(Message msg) {
         return ((JsonObject) (msg.body())).getValue("value");
-    }
-
-    private Gauge score(String playerName) {
-        return hawkular.metricBuilder()
-                .addSegment("source", "falco")
-                .addSegment("player", playerName)
-                .addSegment("metric", "score")
-                .toGauge();
-    }
-
-    private Gauge heat(String playerName) {
-        return hawkular.metricBuilder()
-                .addSegment("source", "falco")
-                .addSegment("player", playerName)
-                .addSegment("metric", "heat")
-                .toGauge();
-    }
-
-    private Gauge level(String playerName) {
-        return hawkular.metricBuilder()
-                .addSegment("source", "falco")
-                .addSegment("player", playerName)
-                .addSegment("metric", "level")
-                .toGauge();
-    }
-
-    private Gauge heatLimit(String playerName) {
-        return hawkular.metricBuilder()
-                .addSegment("source", "falco")
-                .addSegment("player", playerName)
-                .addSegment("metric", "heat-limit")
-                .toGauge();
-    }
-
-    private Logger timeline(String playerName) {
-        return hawkular.metricBuilder()
-                .addSegment("source", "falco")
-                .addSegment("player", playerName)
-                .addSegment("metric", "timeline")
-                .toLogger();
     }
 
     private void generateException() {
@@ -254,7 +189,7 @@ public class GameMonitoring extends AbstractVerticle {
             StringWriter sw = new StringWriter();
             PrintWriter pw = new PrintWriter(sw);
             e.printStackTrace(pw);
-            hawkularLogger.error(e);
+//            hawkularLogger.error(e);
             vertx.eventBus().send("logs", sw.toString());
         }
     }
