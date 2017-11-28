61b7ee4fa283b07769112ba3892e961a62da5192
Joel Takvorian <jtakvori@redhat.com>
2017-11-28 11:09:11 +0100

    Update grafana dashboard, add JMX exporter

--- Dockerfile
+++ Dockerfile
@@ -2,12 +2,14 @@ FROM openjdk:8-jre
 
 WORKDIR "/tmp"
 
-EXPOSE 8081
+EXPOSE 8081 9099
 
 ADD start.sh /tmp/start.sh
 RUN chmod +x /tmp/start.sh
 
 ADD webroot /tmp/webroot
 ADD target/falco-demo.jar /tmp/falco-demo.jar
+ADD jmx_prometheus_javaagent-0.10.jar /tmp/jmx_prometheus_javaagent-0.10.jar
+ADD jmx-config.yml /tmp/jmx-config.yml
 
 ENTRYPOINT ["/tmp/start.sh"]
--- start.sh
+++ start.sh
@@ -8,5 +8,5 @@
 #
 # echo "Hawkular ready!"
 
-java -Dvertx.disableDnsResolver=true -jar falco-demo.jar
+java -Dvertx.disableDnsResolver=true -javaagent:/tmp/jmx_prometheus_javaagent-0.10.jar=9099:/tmp/jmx-config.yml -jar falco-demo.jar
 # java -Dvertx.disableDnsResolver=true -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -DHAWKULAR_HOST=$HAWKULAR_HOST -DUSE_DW_METRICS=true -Dorg.slf4j.simpleLogger.defaultLogLevel=debug -jar falco-demo.jar
